c05f1a8aee430870864ed0855e5ee5e19bf6aef7
Jaynil Patil <jaynilpatil2007@gmail.com>
2026-01-12 12:35:39 +0530

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Components/Header/Header"
+import Footer from "./Components/Footer/Footer"
 
 function App() {
 
   return (
     <>
       <Header />
+      <Footer />
     </>
   )
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,8 +9,8 @@ function Header() {
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
                         <img
-                            src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-                            className="mr-3 h-12"
+                            src="./Images/Logo.jpeg"
+                            className="mr-2 h-12"
                             alt="Logo"
                         />
                     </Link>
